f8a61c9ea16a5b3685fbb65fa488adf11eaafc40
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-08-16 12:10:10 +0100

    Add function to iterate over tasks and display tasks

--- src/index.js
+++ src/index.js
@@ -16,5 +16,23 @@ function Task(description, completed, index) {
 
 function createTasks() {
   const task = new Task('Testing', true, 0);
-  myTasks.push(task);
+  const task1 = new Task('Testing', true, 1);
+  const task2 = new Task('Testing', true, 2);
+  myTasks.push(task, task1, task2);
 }
+
+function displayTasks() {
+  let content = '';
+  for (let i = 0; i < myTasks.length; i += 1) {
+    content = `${myTasks[i].description} ${myTasks[i].completed}, ${myTasks[i].index}`;
+
+    const listItem = document.createElement('p');
+    listItem.innerText = content;
+    mainList.appendChild(listItem);
+  }
+}
+
+window.addEventListener('load', () => {
+  createTasks();
+  displayTasks();
+});
