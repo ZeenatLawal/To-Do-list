341fd731c9fcc06016e32155293f59a8765bca68
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-08-16 14:23:30 +0100

    Add function to iterate over the tasks array

--- src/index.js
+++ src/index.js
@@ -2,37 +2,35 @@ import 'lodash';
 import './style.css';
 
 const mainList = document.getElementById('main-list');
-// const listItem = document.createElement('li');
-// listItem.innerHTML = 'Testing';
-// mainList.appendChild(listItem);
 
-const myTasks = [];
-
-function Task(description, completed, index) {
-  this.description = description;
-  this.completed = completed;
-  this.index = index;
-}
-
-function createTasks() {
-  const task = new Task('Testing', true, 0);
-  const task1 = new Task('Testing', true, 1);
-  const task2 = new Task('Testing', true, 2);
-  myTasks.push(task, task1, task2);
-}
+const myTasks = [{
+  description: 'Read ES6 Modules',
+  completed: true,
+  index: 0,
+},
+{
+  description: 'Read Webpack',
+  completed: true,
+  index: 1,
+},
+{
+  description: 'List structure project',
+  completed: false,
+  index: 2,
+}];
 
 function displayTasks() {
   let content = '';
   for (let i = 0; i < myTasks.length; i += 1) {
-    content = `${myTasks[i].description} ${myTasks[i].completed}, ${myTasks[i].index}`;
+    content = `${myTasks[i].description}`;
 
-    const listItem = document.createElement('p');
+    const listItem = document.createElement('li');
     listItem.innerText = content;
+    listItem.className = 'list-item';
     mainList.appendChild(listItem);
   }
 }
 
 window.addEventListener('load', () => {
-  createTasks();
   displayTasks();
 });
