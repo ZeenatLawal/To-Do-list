c4b927a40398f0c05e17ec38e5aa934a42a75137
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-08-25 19:22:59 +0100

    Add Tests For deleteTask Function

--- add_remove.test.js
+++ add_remove.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { addTask } from './src/add_remove.js';
+import { addTask, deleteTask } from './src/add_remove.js';
 
 describe('adding a task to the list', () => {
   document.body.innerHTML = '<input type="text" placeholder="Add to your list..." id="inputList">';
@@ -27,3 +27,42 @@ describe('adding a task to the list', () => {
     expect(myTasks[1].index).toBe(2);
   });
 });
+
+
+describe('deleting a task from the list', () => {
+  const myTasks = [
+    {
+    description: 'Walk The Dog',
+    completed: false,
+    index: 1,
+    },
+
+    {
+    description: 'Do Laundry',
+    completed: false,
+    index: 2,
+    },
+
+    {
+    description: 'Join Stand-up',
+    completed: false,
+    index: 3,
+    },
+
+    {
+    description: 'Write Elevator Pitch',
+    completed: false,
+    index: 4,
+    },
+
+  ];
+
+  test('Delete task with index 2', () => {
+    deleteTask(myTasks, 2);
+    expect((myTasks)).toHaveLength(3);
+  });
+
+  test('Update index after task deletion', () => {
+    expect(myTasks[2].index).toBe(3);
+  });
+})
