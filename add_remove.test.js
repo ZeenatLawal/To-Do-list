1c2e62b99c565d4c4132e510cbf94c3e395f5b25
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-08-25 17:12:09 +0100

    Add Test for Adding Tasks

--- add_remove.test.js
+++ add_remove.test.js
@@ -1,18 +1,49 @@
+/**
+ * @jest-environment jsdom
+ */
 import { addTask } from './src/add_remove.js';
+
+// class LocalStorageMock {
+//   constructor() {
+//     this.store = {};
+//   }
+
+//   clear() {
+//     this.store = {};
+//   }
+
+//   getItem(key) {
+//     return this.store[key] || null;
+//   }
+
+//   setItem(key, value) {
+//     this.store[key] = String(value);
+//   }
+
+//   removeItem(key) {
+//     delete this.store[key];
+//   }
+// };
+
+// global.localStorage = new LocalStorageMock;
+
 
 describe('adding a task to the list', () => {
   test('Add one new item to the list', () => {
-    const task = {
-      description: 'Read webpack',
-      completed: false,
-      index: 1,
-    };
-    document.body.innerHTML = '<div>'
-      + '  <ul id="main-list"></li>'
-      + '</div>';
+    document.body.innerHTML = `<div class="card">
+      <div class="list-head">
+        <input type="text" placeholder="Add to your list..." id="inputList">
+      </div>
+      <div>
+        <ul id="main-list">
+        </ul>
+      </div>
+    </div>`;
+    // const inputList = document.getElementById('inputList');
+    // inputList.value = 'Zeenat';
+    const task = [];
     addTask(task);
-    const list = document.querySelectorAll('#list li');
-    expect(list).toHaveLength(1);
+    expect(task).toHaveLength(1);
   });
 });
 
